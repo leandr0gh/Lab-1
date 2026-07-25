4ffb4d408450822a631b19eb610eb395dde02ee7
leandr0gh <leandrosandoval131205@gmail.com>
2026-07-25 11:51:00 -0600

    Crear clase MainApp, agregar panel botones asientos Leandro/ian

---
+++ Laboratorio 1/src/lab1/MainApp.java
@@ -0,0 +1,40 @@
+
+package lab1;
+
+import javax.swing.*;
+import java.awt.*;
+public class MainApp extends JFrame {
+    
+    private PalindromoAir avion=new PalindromoAir();
+    private JButton[] botonesAsiento=new JButton[30];
+    private JTextField campoNombre=new JTextField(15);
+    private JTextArea consola=new JTextArea();
+    
+    
+
+
+    public MainApp() {
+        setTitle("PALINDROMO AIR");
+        setSize(1024, 768);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        JPanel panelAsiento=new JPanel(new GridLayout(6,5,5,5));
+        for (int i = 0; i < 30; i++) {
+            JButton boton=new JButton("Asiento "+(i+1));
+            botonesAsiento[i]= boton;
+            panelAsiento.add(boton);
+        }
+        add(panelAsiento, BorderLayout.WEST);
+        
+        
+        //botones
+       
+        
+        setVisible(true);
+    }
+    
+
+    public static void main(String[] args){
+    
+    }
+}

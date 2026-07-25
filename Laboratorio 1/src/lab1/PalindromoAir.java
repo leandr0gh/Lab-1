5fb7d14374b4e4c86e743d0492f26e96bd10f094
DEaniel Kattan <dagkattan221608@gmail.com>
2026-07-25 09:58:59 -0600

    Agregados metodo sellTicket, cancelTicket y dispatch

--- Laboratorio 1/src/lab1/PalindromoAir.java
+++ Laboratorio 1/src/lab1/PalindromoAir.java
@@ -16,4 +16,51 @@ public class PalindromoAir {
             return FirstAvailable(index+1);
         }
    return index; }
+    public int sellTicket(String nombre) {
+        int pos = FirstAvailable(0);
+
+        if (pos == -1) {
+            System.out.println("No hay asientos disponibles. El avion va lleno.");
+            return -1;
+        }
+
+        boolean esPali = isPalindromo(nombre);
+        double montoOriginal = 150.00;
+        double montoFinal = montoOriginal;
+
+        if (esPali) {
+            montoFinal = montoOriginal - (montoOriginal * 0.20);
+        }
+
+        Ticket ticket = new Ticket(nombre, montoOriginal, montoFinal, esPali);
+        asientos[pos] = ticket;
+
+        System.out.println("Ticket vendido a " + nombre + " en el asiento " + (pos + 1));
+        System.out.println("Monto pagado: L" + montoFinal + (esPalindromo ? " (con descuento por palindromo)" : ""));
+
+        return pos;
+    }
+
+    public boolean cancelTicket(String nombre) {
+        int pos = searchPassenger(nombre, 0);
+
+        if (pos == -1) {
+            System.out.println("No se encontro ningun pasajero con el nombre " + nombre);
+            return false;
+        }
+
+        asientos[pos] = null;
+        System.out.println("Se cancelo el ticket de " + nombre + ", asiento " + (pos + 1) + " libre.");
+        return true;
+    }
+
+    public void dispatch() {
+        double total = income(0);
+        System.out.println("Despacho del vuelo");
+        System.out.println("Ingresos totales: L" + total);
+
+        reset(0);
+        System.out.println("Avion limpio, todos los asientos estan libres otra vez.");
+    }
 }
+

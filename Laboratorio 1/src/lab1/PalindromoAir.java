38841610a201fb1555549f706f8b42b761d66d37
leandr0gh <leandrosandoval131205@gmail.com>
2026-07-25 09:51:16 -0600

    Agregar metodo searchPassenger Leandro

--- Laboratorio 1/src/lab1/PalindromoAir.java
+++ Laboratorio 1/src/lab1/PalindromoAir.java
@@ -1,4 +1,3 @@
-
 package lab1;
 
 /**
@@ -6,14 +5,31 @@ package lab1;
  * @author Leandro
  */
 public class PalindromoAir {
-    Ticket[] asientos=new Ticket[30];
-    
-    public int FirstAvailable(int index){
-        if (index>=30) {
+
+    Ticket[] asientos = new Ticket[30];
+
+    public int FirstAvailable(int index) {
+        if (index >= 30) {
             return -1;
         }
-        if (asientos[index]!=null) {
-            return FirstAvailable(index+1);
+        if (asientos[index] != null) {
+            return FirstAvailable(index + 1);
         }
-   return index; }
+        return index;
+    }
+
+    public int searchPassenger(String nombre, int index) {
+        if (index >= 30) {
+            return -1;
+        }
+
+        if (asientos[index] == null) {
+            return searchPassenger(nombre, index + 1);
+        } else if (asientos[index].getNombre().equals(nombre)) {
+
+            return index;
+        }
+
+        return searchPassenger(nombre, index+1);
+    }
 }
